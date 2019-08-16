39bb3832d651dea16b35a31d1d3d6c05c0df3d39
lerbuke <l.erbuke@bluetimeconcept.ch>
2019-08-16 13:13:26 +0200

    Cleanup.

--- vtv_create_entries.py
+++ vtv_create_entries.py
@@ -1,37 +1,25 @@
 #!/usr/bin/env python
 """
 vtv_create_entries.py
-Read the csv file and create corresponding entries in database
+Read a csv file to create corresponding entries in database.
+auto: automate
+cad: camera
+pha: phare
+dis: disjoncteur
 """
 import argparse, os, sys
 import sqlite3
 
-parser = argparse.ArgumentParser(description='Création des données dans la base vtv.')
+parser = argparse.ArgumentParser(description='Création des données dans la base server0.db du projet VTV.')
 parser.add_argument('-fn', '--file_name', default='vtv_instances.csv', help='Nom du fichier csv')
+parser.add_argument('-dbp', '--db_path', default='db/', help='Chemin de la base de données server0.db')
 
 args = parser.parse_args()
 print(args)
 
-
 try:
+    # Open the file and create automate and camera lists
     fw = open(args.file_name, "r")
-    conn = sqlite3.connect('db\server0.db')
-
-   
-    cur = conn.cursor()
-    # Delete all MODBUS (0) and SNMP (3) providers, provider configurations and replies
-    for provider in (0,3):
-        cur.execute("SELECT providerNbSubId FROM provider WHERE providerType={}".format(provider))
-        nb = cur.fetchone()[0]
-
-        range = (0, nb-1)
-        for i in range: 
-            suffix = "_" + str(provider) + "_" + str(i)
-            cur.execute('DROP TABLE IF EXISTS provider' + suffix)
-            cur.execute('DROP TABLE IF EXISTS provider_config' + suffix)
-            cur.execute('DROP TABLE IF EXISTS provider_reply' + suffix)
-        print(cur.fetchone())
-       
     CAMERA = 'cad'
     AUTOMATE = 'aut'
     cameras = []
@@ -43,13 +31,101 @@ try:
         if AUTOMATE in line:
             automates.append(value)
     fw.close()
+    
+    # Open the server0.db database
+    conn = sqlite3.connect(args.db_path + 'server0.db')  
+    cur = conn.cursor()
+    
+    # Delete all MODBUS (0) and SNMP (3) providers, provider configurations and replies
+    for provider in (0,3):
+        cur.execute("SELECT providerNbSubId FROM provider WHERE providerType={}".format(provider))
+        nb = cur.fetchone()[0]
 
-
+        range = (0, nb-1)
+        for i in range: 
+            suffix = "_" + str(provider) + "_" + str(i)
+            cur.execute('DROP TABLE IF EXISTS provider' + suffix)
+            cur.execute('DROP TABLE IF EXISTS provider_config' + suffix)
+            cur.execute('DROP TABLE IF EXISTS provider_reply' + suffix)       
+   
+    # Update the number of automates and cameras
     cur.execute("UPDATE provider SET providerNbSubId = {} WHERE providerType = 0".format(len(automates)))
     cur.execute("UPDATE provider SET providerNbSubId = {} WHERE providerType = 3".format(len(cameras)))
-
+      
     k = 0
-    sys.stdout.write('Creating camera entries...')
+    sys.stdout.write('Creating {} automate entries...'.format(len(automates)))    
+    for aut in automates:
+        cur.execute("""CREATE TABLE "provider_0_{}" (
+            "id" INTEGER NOT NULL,
+            "tag" TEXT,
+            "desc" TEXT,
+            "variant" INTEGER,
+            "attribute" INTEGER,
+            "alarm" INTEGER,
+            "prio" INTEGER,
+            "alarm_txt" INTEGER,
+            "invert" INTEGER,
+            "renv0" INTEGER,
+            "renv1" INTEGER,
+            "renv2" INTEGER,
+            "renv3" INTEGER,
+            "renv4" INTEGER,
+            "immediate" INTEGER,
+            "store" INTEGER,
+            "scale" REAL,
+            "factor" REAL,
+            "field0" TEXT,
+            "field1" TEXT,
+            "texpro" integer,
+            PRIMARY KEY ("id")
+            );""".format(k))
+        
+        cur.execute("""CREATE TABLE "provider_config_0_{}" (
+            "id" INTEGER NOT NULL,
+            "name" TEXT,
+            "field0" TEXT,
+            "field1" TEXT,
+            "field2" TEXT,
+            "field3" TEXT,
+            "field4" TEXT,
+            "field5" TEXT,
+            PRIMARY KEY ("id")
+            );""".format(k))
+        
+        cur.execute("""CREATE TABLE "provider_reply_0_{}" (
+            "id" INTEGER NOT NULL,
+            "tag" TEXT,
+            "desc" TEXT,
+            "variant" INTEGER,
+            "alarm" INTEGER,
+            "prio" INTEGER,
+            "alarm_txt" INTEGER,
+            "renv0" INTEGER,
+            "renv1" INTEGER,
+            "renv2" INTEGER,
+            "renv3" INTEGER,
+            "renv4" INTEGER,
+            "store" INTEGER,
+            "texpro" integer,
+            PRIMARY KEY ("id")
+            );""".format(k))
+            
+        conn.commit()
+        
+        cur.execute("INSERT INTO provider_0_{} (tag,desc,variant,attribute,alarm,invert,field0,field1,texpro) VALUES('dis_{}_sti','Défaut disjoncteur',2,1,1,1,1,0,1)".format(k,aut))
+        conn.commit()
+        cur.execute("INSERT INTO provider_0_{} (tag,desc,variant,attribute,renv4,field0,field1) VALUES('pha_{}_cdi','Commande dallumage du phare infrarouge',2,0,{},0,0)".format(k,aut,k+1))
+        conn.commit()
+        cur.execute("INSERT INTO provider_0_{} (tag,desc,variant,attribute,alarm,field0,field1,texpro) VALUES('pha_{}_sti','Etat Phare IR',2,1,1,0,0,1)".format(k,aut))
+        cur.execute("INSERT INTO provider_config_0_{} (name,field1,field2,field3) VALUES('aut_{}',502,500,1)".format(k,aut))
+        cur.execute("INSERT INTO provider_reply_0_{} (tag,desc,variant,alarm,renv0,renv1,renv2,texpro) VALUES('aut_{}_dsc','Défaut com. Automate',2,1,1,1,{},1)".format(k,aut,k+1))
+    
+        k+=1
+        sys.stdout.write('.')
+    print('Done.')
+    
+    k = 0
+    sys.stdout.write('Creating {} camera entries...'.format(len(cameras)))
     for cam in cameras:
         cur.execute("""CREATE TABLE 'provider_3_{}' (
             'id' INTEGER NOT NULL,
@@ -114,83 +190,7 @@ try:
         k+=1
         sys.stdout.write('.')
     print('Done.')
-    
-    k = 0
-    for aut in automates:
-        cur.execute("""CREATE TABLE "provider_0_{}" (
-            "id" INTEGER NOT NULL,
-            "tag" TEXT,
-            "desc" TEXT,
-            "variant" INTEGER,
-            "attribute" INTEGER,
-            "alarm" INTEGER,
-            "prio" INTEGER,
-            "alarm_txt" INTEGER,
-            "invert" INTEGER,
-            "renv0" INTEGER,
-            "renv1" INTEGER,
-            "renv2" INTEGER,
-            "renv3" INTEGER,
-            "renv4" INTEGER,
-            "immediate" INTEGER,
-            "store" INTEGER,
-            "scale" REAL,
-            "factor" REAL,
-            "field0" TEXT,
-            "field1" TEXT,
-            "texpro" integer,
-            PRIMARY KEY ("id")
-            );""".format(k))
-        
-        cur.execute("""CREATE TABLE "provider_config_0_{}" (
-            "id" INTEGER NOT NULL,
-            "name" TEXT,
-            "field0" TEXT,
-            "field1" TEXT,
-            "field2" TEXT,
-            "field3" TEXT,
-            "field4" TEXT,
-            "field5" TEXT,
-            PRIMARY KEY ("id")
-            );""".format(k))
-        
-        cur.execute("""CREATE TABLE "provider_reply_0_{}" (
-            "id" INTEGER NOT NULL,
-            "tag" TEXT,
-            "desc" TEXT,
-            "variant" INTEGER,
-            "alarm" INTEGER,
-            "prio" INTEGER,
-            "alarm_txt" INTEGER,
-            "renv0" INTEGER,
-            "renv1" INTEGER,
-            "renv2" INTEGER,
-            "renv3" INTEGER,
-            "renv4" INTEGER,
-            "store" INTEGER,
-            "texpro" integer,
-            PRIMARY KEY ("id")
-            );""".format(k))
-            
-        conn.commit()
-        
-        cur.execute("INSERT INTO provider_0_{} (tag,desc,variant,attribute,alarm,invert,field0,field1,texpro) VALUES('dis_{}_sti','Défaut disjoncteur',2,1,1,1,1,0,1)".format(k,aut))
-        conn.commit()
-        cur.execute("INSERT INTO provider_0_{} (tag,desc,variant,attribute,renv4,field0,field1) VALUES('pha_{}_cdi','Commande dallumage du phare infrarouge',2,0,{},0,0)".format(k,aut,k+1))
-        conn.commit()
-        cur.execute("INSERT INTO provider_0_{} (tag,desc,variant,attribute,alarm,field0,field1,texpro) VALUES('pha_{}_sti','Etat Phare IR',2,1,1,0,0,1)".format(k,aut))
-        conn.commit()
-        cur.execute("INSERT INTO provider_config_0_{} (name,field1,field2,field3) VALUES('aut_{}',502,500,1)".format(k,aut))
-        cur.execute("INSERT INTO provider_reply_0_{} (tag,desc,variant,alarm,renv0,renv1,renv2,texpro) VALUES('aut_{}_dsc','Défaut com. Automate',2,1,1,1,{},1)".format(k,aut,k+1))
-    
-        k+=1
-        sys.stdout.write('.')
-    print('Done.')
-    
-    #for cam in cameras:    
-    print(len(cameras))
-    print(cameras)
-    print(automates)
+   
     conn.commit()
     conn.close()
 except IOError as e:
