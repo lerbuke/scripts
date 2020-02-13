97d89a00932920be06b3b6f0d022d2e8b0fbf4aa
lerbuke <l.erbuke@bluetimeconcept.ch>
2020-02-13 15:01:47 +0100

    Updated.

--- cernbursins/cb_create_entries.py
+++ cernbursins/cb_create_entries.py
@@ -23,21 +23,21 @@ try:
         if value[0] != '' and value[0] != '(*':
             list.append(value)
     fw.close()
-    
+
     #for item in list:
      #   print(item)
-    
- 
-    
+
     # Open the server0.db database
     conn = sqlite3.connect(args.db_path + 'server0.db')
     cur = conn.cursor()
- 
+
+    cur.execute('DELETE FROM local')
     cur.execute('DROP TABLE IF EXISTS provider_0_0')
     cur.execute("""CREATE TABLE "provider_0_0" (
         "id" INTEGER NOT NULL,
         "tag" TEXT,
         "desc" TEXT,
+        "desc2" TEXT,
         "variant" INTEGER,
         "attribute" INTEGER,
         "alarm" INTEGER,
@@ -55,56 +55,76 @@ try:
         "factor" REAL,
         "field0" TEXT,
         "field1" TEXT,
+        "field2" TEXT,
         "texpro" integer,
         PRIMARY KEY ("id"));""")
     conn.commit()
-    
-    for item in list:    
+    jour_mois = 1
+    heure_min = 1
+    compteur = 1
+    for item in list:
         if len(item) >= 3:
-            desc = ''
             addr_numbers = re.findall(r'[0-9]+', item[1]) # build a list of number found item[1]
-            if len(addr_numbers) == 2:
-                if 'BOOL' == item[2]:
-                    # ['TelerelaisEclairagePubliqueModeNuit', 'AT %MX4.0', BOOL']                
-                    address = 12288 + 8 * int(addr_numbers[0]) + int(addr_numbers[1])
-                    cur.execute("INSERT INTO provider_0_0 (tag,desc,variant,attribute,alarm,field0,field1,texpro) VALUES('{}','{}',2,0,0,0,'{}',0)".format(item[0], desc, address))
-                    
-                elif 'BYTE' == item[2]:  
-                    # ['TS04_Statuts', 'AT %MB400', 'BYTE', '(* Pointeur pour la comm ADS *)']
-                    if len(item) == 4:
-                        desc=item[3]
-                    
-                elif 'INT' == item[2]:
-                    # ['DureeTestEclairageSecours', 'AT %MW20', 'INT']
-                    pass
-                    
-                elif 'DINT' == item[2]:
-                    # ['TS02_Compteur1', 'AT %MB210', 'DINT']
-                    pass
-                    
-                elif 'WORD' == item[2]:
-                    # ['PulseComptageTener', 'AT%MB1000', 'WORD']
-                    pass
-                    
-                elif 'DT' == item[2]:
-                    # ['DateEtHeure', 'AT %MD0', 'DT']
-                    pass
-                    
-                elif 'JourMois' == item[2]:
-                    # ['Ferie02', 'AT %MW802', 'JourMois']
-                    pass
-                    
-                elif 'HeureMinute' == item[2]:
-                    # ['HeurePortesProg1EteOff1', 'AT %MW1500', 'HeureMinute', '(* Programme du mation du lundi au jeudi *)']
-                    if len(item) == 4:
-                        desc=item[3]
-                    
+            if 'BOOL' == item[2]:
+                # ['TelerelaisEclairagePubliqueModeNuit', 'AT %MX4.0', BOOL']
+                if int(addr_numbers[0]) % 2 == 0:
+                    address = 12288 + int(int(addr_numbers[0]) / 2)
+                    cur.execute("INSERT INTO provider_0_0 (tag,variant,attribute,field0,field1,field2,alarm) VALUES('{}',2,0,2,'{}','{}',1)".format(item[0], address, int(addr_numbers[1])+1))
                 else:
-                    print('Unknown type: ', item[2])
-                
+                    address = 12288 + int((int(addr_numbers[0])-1) / 2)
+                    cur.execute("INSERT INTO provider_0_0 (tag,variant,attribute,field0,field1,field2,alarm) VALUES('{}',2,0,2,'{}','{}',1)".format(item[0], address, int(addr_numbers[1])+8+1))
+
+            elif 'BYTE' == item[2]:
+                # ['TS04_Statuts', 'AT %MB400', 'BYTE', '(* Pointeur pour la comm ADS *)']
+                pass
+
+            elif 'INT' == item[2]:
+                # ['DureeTestEclairageSecours', 'AT %MW20', 'INT']
+                address = 12288 + int(int(addr_numbers[0]) / 2)
+                cur.execute("INSERT INTO provider_0_0 (tag,variant,attribute,field0,field1) VALUES('{}',1,0,2,'{}')".format(item[0], address))
+                pass
+
+            elif 'DINT' == item[2]:
+                # ['TS02_Compteur1', 'AT %MB210', 'DINT']
+                address = 12288 + int(int(addr_numbers[0]) / 2)
+                cur.execute("INSERT INTO provider_0_0 (tag,variant,attribute,field0,field1,renv0,renv1,renv2) VALUES('{}_LSB',1,0,2,'{}',3,1,{})".format(item[0], address, compteur))
+                cur.execute("INSERT INTO provider_0_0 (tag,variant,attribute,field0,field1,renv0,renv1,renv2) VALUES('{}_MSB',1,0,2,'{}',3,2,{})".format(item[0], address+1, compteur))
+                cur.execute("INSERT INTO local (tag,variant,renv0,renv1,renv2) VALUES('{}',1,3,3,{})".format(item[0], compteur))
+                compteur = compteur + 1
+                pass
+
+            elif 'WORD' == item[2]:
+                # ['PulseComptageTener', 'AT%MB1000', 'WORD']
+                pass
+
+            elif 'DT' == item[2]:
+                # ['DateEtHeure', 'AT %MD0', 'DT']
+                pass
+
+            elif 'JourMois' == item[2]:
+                # ['Ferie02', 'AT %MW802', 'JourMois']
+                address = 12288 + int(int(addr_numbers[0]) / 2)
+                cur.execute("INSERT INTO provider_0_0 (tag,variant,attribute,field0,field1,renv0,renv1,renv2) VALUES('{}',1,0,2,'{}',1,1,{})".format(item[0], address, jour_mois))
+                cur.execute("INSERT INTO local (tag,variant,renv0,renv1,renv2) VALUES('{}_Jour',1,1,2,{})".format(item[0], jour_mois))
+                cur.execute("INSERT INTO local (tag,variant,renv0,renv1,renv2) VALUES('{}_Mois',1,1,3,{})".format(item[0], jour_mois))
+                jour_mois = jour_mois + 1
+                pass
+
+            elif 'HeureMinute' == item[2]:
+                # ['HeurePortesProg1EteOff1', 'AT %MW1500', 'HeureMinute', '(* Programme du matin du lundi au jeudi *)']
+                address = 12288 + int(int(addr_numbers[0]) / 2)
+                cur.execute("INSERT INTO provider_0_0 (tag,variant,attribute,field0,field1,renv0,renv1,renv2) VALUES('{}',1,0,2,'{}',2,1,{})".format(item[0], address, heure_min))
+                cur.execute("INSERT INTO local (tag,variant,renv0,renv1,renv2) VALUES('{}_Heure',1,2,2,{})".format(item[0], heure_min))
+                cur.execute("INSERT INTO local (tag,variant,renv0,renv1,renv2) VALUES('{}_Min',1,2,3,{})".format(item[0], heure_min))
+                heure_min = heure_min + 1
+
+            else:
+                #print('Unknown type: ', item[2])
+                pass
+
     conn.commit()
     conn.close()
-    
+
 except IOError as e:
    print("I/O error({0}): {1}".format(e.errno, e.strerror))
    print(traceback.format_exc())
@@ -120,5 +140,6 @@ except: #handle other exceptions such as attribute errors
    print(traceback.format_exc())
    os.system("pause")
    exit()
-   
+
 print("Done.")
+os.system("pause")
